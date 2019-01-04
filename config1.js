1d2c5f7b007b23354daa0835b7f28688209e7086
illusiveo <46373282+illusiveo@users.noreply.github.com>
2019-01-04 14:28:04 +0300

    Add files via upload

---
+++ config1.js
@@ -0,0 +1,5 @@
+exports.TOKEN = 'process.env.BOT_TOKEN'; // NTMwNzA3OTY1OTU1NTM4OTU0.DxDUBw.KHtMgMDlg_0tLD0MSvz1ogPKC9U
+
+exports.PREFIX = '2';//1
+
+exports.GOOGLE_API_KEY = 'AIzaSyAdORXg7UZUo7sePv97JyoDqtQVi3Ll0b8';
